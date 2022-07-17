cbf14e379b8735038942cbf34f8a4095df682be1
aladin002dz <mahfoudh.arous@gmail.com>
2022-07-17 11:22:39 +0100

    js examples, more exaplanation of variables, arrays and functions

--- js-examples/2-conditions-loops/index.js
+++ js-examples/2-conditions-loops/index.js
@@ -1,5 +1,11 @@
 console.log("===========================");
 //example about if else in javascript
+// if(condition){
+//     //code to execute if condition is true
+// } else {
+//     //code to execute if condition is false
+// }
+
 var age = 30;
 if (age > 18) {
   console.log("You are old enough");
@@ -8,9 +14,10 @@ if (age > 18) {
 }
 console.log("===========================");
 
-console.log("===========================");
 //example about loops in javascript
+//We use the loop when we want to do something a number of times
 for (var i = 0; i < 10; i++) {
+  //i is the variable, 0 is the start, 10 is the end, and i++ is the increment
   console.log(i + "- I love javascript");
 }
 console.log("===========================");
--- js-examples/3-functions/js/index.js
+++ js-examples/3-functions/js/index.js
@@ -3,23 +3,43 @@ function add(a, b) {
   return a + b;
 }
 var result = add(1, 2);
-console.log("Add===========================");
+console.log("1+2===========================");
 console.log(result); //output: 3
-console.log("===========================");
+
+console.log("CelciusToFahrenheit===========================");
 function celciusToFahrenheit(c) {
   return (c * 9) / 5 + 32;
 }
-var result = celciusToFahrenheit(30);
-console.log("CelciusToFahrenheit===========================");
+
+//30° C to Fahrenheit
+var celcius = 30;
+result = celciusToFahrenheit(celcius);
+console.log("30° C to Fahrenheit===========================");
 console.log(result); //output: 86
-console.log("===========================");
+
+//12° C to Fahrenheit
+celcius = 12;
+console.log("12° C to Fahrenheit===========================");
+result = celciusToFahrenheit(celcius);
+console.log(result); //output: 54
+
+console.log("DaysToSeconds===========================");
 function daysToSeconds(days) {
   var daysToHours = days * 24;
   var hoursToMinutes = daysToHours * 60;
   var minutesToSeconds = hoursToMinutes * 60;
   return minutesToSeconds;
 }
-var result = daysToSeconds(1);
-console.log("DaysToSeconds===========================");
+
+//Seconds in 1 days
+var days = 1;
+console.log("Seconds in 1 Day===========================");
+result = daysToSeconds(days);
 console.log(result); //output: 86400
+
+//Seconds in 1 week
+days = 7;
+result = daysToSeconds(days);
+console.log("Seconds in 1 Week===========================");
+console.log(result); //output: 604800
 console.log("===========================");
